41daf1909371eed062e0045f932235b816024556
KevinMenden <kevin.menden@t-online.de>
2018-10-23 09:55:50 +0200

    Updated parsers

--- main.py
+++ main.py
@@ -12,9 +12,19 @@ if __name__=='__main__':
     parser.add_argument("--mode", type=str, help="Whether to train or generate images. One of [train | gen]", default="train")
     parser.add_argument("--model_dir", type=str, help="The model directory", default="./")
     parser.add_argument("--n_img", type=int, help="Number of images to generate. Default: 32", default=32)
+    parser.add_argument("--n_steps", type=int, help="Number of training steps. Default: 1000", default=1000)
+    # Parse arguments
+    args = parser.parse_args()
+    mode = args.mode
+    model_dir = args.model_dir
+    n_images = args.n_img
+    
 
-    model_dir = "/home/kevin/models/vae"
 
+    # Training mode
+    if mode == "train":
+        train_model(model_dir=model_dir,
+                    num_steps=)
     # model training
     train_model(model_dir=model_dir,
                 num_steps=1000,
